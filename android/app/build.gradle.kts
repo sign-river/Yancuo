d8b5a1075f838ad490fe6978777e97cd0321ef2a
sign-river <3217344726@qq.com>
2026-08-03 03:47:20 +0800

    build(android): stabilize encrypted preferences dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.security:security-crypto:1.1.0")
 
     implementation("io.coil-kt:coil-compose:2.5.0")
 
